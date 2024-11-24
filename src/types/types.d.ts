d0a8c9eefbbe8d05a4ccdac8a1e13d01bc14d224
Ian Dorion <ian@idecentralize.finance>
2024-11-24 04:03:18 -0500

    types export

---
+++ src/types/types.d.ts
@@ -0,0 +1,1 @@
+declare module '@idecentralize-finance/chain-types';
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "node:path";
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+    dts({
+      entryRoot: 'src',
+      outputDir: 'dist',
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
